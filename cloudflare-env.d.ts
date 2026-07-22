9c7f1d5fa349a207557dd02a8575e597568e3aab
caizilong <59225086+caizilong@users.noreply.github.com>
2026-07-22 22:27:09 +0800

    Deploy Blindfolded on Cloudflare Workers (#1)
    * deploy Blindfolded on Cloudflare Workers
    
    * clarify deployment documentation

--- cloudflare-env.d.ts
+++ cloudflare-env.d.ts
@@ -2,6 +2,8 @@ interface Fetcher {
   fetch(input: RequestInfo | URL, init?: RequestInit): Promise<Response>;
 }
 
+// This project does not use D1 yet; keep a placeholder for generated Worker types.
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 interface D1Database {}
 
 declare module "cloudflare:workers" {
